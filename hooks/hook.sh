448974176b2a64748134474fbd3292b1df2c5b2a
Savindu Gunawardena <savibashnon11@gmail.com>
2021-05-24 04:40:08 +0530

    remove bashscript commends

--- hooks/hook.sh
+++ hooks/hook.sh
@@ -6,35 +6,34 @@ githubAuthToken="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"
 githubUser="savindug"
 githubRepo="hotel-revenue-service"
 
-# First, get the zip file
 cd $serverRoot && sudo curl -O -J -L -u $githubAuthToken:x-oauth-basic https://github.com/$githubUser/$githubRepo/archive/master.zip
 
-# Second, unzip it, if the zip file exists
+
 if [ -f $serverRoot/hotel-revenue-service-master.zip ]; then
-    # Unzip the zip file
+    
     sudo unzip -q $serverRoot/hotel-revenue-service-master.zip
 
-    # Delete zip file
+    
     sudo rm $serverRoot/hotel-revenue-service-master.zip
 
-    #stop currently runnig pm2 server
+ 
     pm2 stop ratebuckets-server
 
-    # Delete current directory
+    
     sudo rm -rf $projectDir
 
-    # Replace with new files
+    
     sudo mv hotel-revenue-service-master $projectDir
 
-    #navigate into project dir
+   
     cd $projectDir
 
-    #install project dependencies
+   
     sudo npm install
 
-    #run new build on pm2
+   
     pm2 start --name ratebuckets-server sudo npm -- start
 
-    #save pm2
+    
     sudo pm2 save
 fi
